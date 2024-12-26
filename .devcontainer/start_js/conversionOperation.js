eeeb439af8cd8634ceb292853b8f8ee1c7138851
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2024-12-26 09:12:42 +0000

    Conversion of datatypes

---
+++ .devcontainer/start_js/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "Vishnu"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumaber = Number(score)
+// console.log(typeof valueInNumaber);
+// console.log (valueInNumaber);
+
+let isLoggedIn = 1
+
+// let booleanInLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanInLoggedIn);
+
+let someNumber = 44
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
