675e3ba2ba624c7d793a07ac404a12e8ebf4fdfb
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2024-12-27 09:29:14 +0000

    String.js

---
+++ .devcontainer/start_js/String.js
@@ -0,0 +1,11 @@
+// String
+const name = "Vishnu"
+const repoCount = 100
+
+console.log(`Hello my name is ${name} are my repo count is ${repoCount}`);
+
+const gameName = new String (`Vishnu`)
+console.log(gameName [0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
