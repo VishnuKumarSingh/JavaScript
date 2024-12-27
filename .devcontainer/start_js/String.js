d7c9c8f5ecaeac4f42d0dc0642128ae7950314ce
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2024-12-27 10:43:41 +0000

    String.js

--- .devcontainer/start_js/String.js
+++ .devcontainer/start_js/String.js
@@ -9,3 +9,20 @@ console.log(gameName [0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf(`n`));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-4, 4)
+console.log(anotherString);
+
+const newStringOne = "    Vishnu   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const flex = "Vishnu"
+console.log(flex.replace(`n`, `-`));
