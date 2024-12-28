a726490c188d7024503264bd732706cc1da94b68
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2024-12-28 07:15:55 +0000

    Numbers.js

---
+++ .devcontainer/start_js/Numbers.js
@@ -0,0 +1,48 @@
+//Numbers----------------------
+
+/*const Score = 500
+console.log(Score);
+
+const Balance = new Number(700)
+console.log(Balance);
+
+console.log(Balance.toString().length);
+
+console.log(Balance.toFixed(2));  //vvi
+
+const anotherNumber = 123.9876
+console.log(anotherNumber.toPrecision(4));
+
+const Hundreds = 100000     //Indian formate to Convert  Importence Line
+console.log(Hundreds.toLocaleString(`en-In`));
+*/
+
+
+
+//Maths---------------------
+
+console.log(Math);
+
+console.log(Math.abs(-5));
+
+console.log(Math.round(9.9));
+
+console.log(Math.ceil(5.9));
+
+console.log(Math.floor(9.2));
+
+console.log(Math.min(4, 5, 6, 7, 3));
+
+console.log(Math.max(4, 5, 33, 44, 6, 8));
+
+console.log(Math.random());    // 0 or 1 ke bich me hi value rehta h
+
+console.log(Math.random()*10 +1);  //  0 na aaye uske liye ham * 10 + 1 kar dete h
+
+console.log(Math.random()*10 +1);
+
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+console.log(math.floor(Math.rendom() * (max - min + 1)) + min);
