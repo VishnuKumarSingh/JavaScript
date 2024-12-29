44fa4503994d1d897a1384111846185edd2a35ad
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2024-12-29 03:08:17 +0000

    Array_Function.js

---
+++ .devcontainer/Array_js/Array_Function.js
@@ -0,0 +1,29 @@
+// const marvel_heros = ["thor", "Ironman", "Spaiderman"]
+// const dc_heros = ["Superman", "Flash", "Batman"]
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+
+  // const all_Heros = marvel_heros.concat(dc_heros)
+  // console.log(all_Heros);
+
+//  const all_new_heros = [...marvel_heros, ...dc_heros]
+//  console.log(all_new_heros);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5], 8, 7, [8, 2, 7, 9 ,0 ,3]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Vishnu"));
+// console.log(Array.from("vishnu"));
+//console.log(Array.from({name: "Vishnu"}));
+
+let Score1 = 100
+let Score2 = 200
+let Score3 = 300
+console.log(Array.of(Score1, Score2, Score3));
