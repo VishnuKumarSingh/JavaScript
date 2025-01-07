ff2effa0eb0d08e89575e89de9b85fb91d22a3a9
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2025-01-07 08:48:31 +0000

    this.js

--- .devcontainer/This_js/this.js
+++ .devcontainer/This_js/this.js
@@ -20,9 +20,36 @@
 // }
 // chai()
 
-function chai(){
-    let name = "Vishnu"
-    console.log(this.name);
-}
-chai()
+// function chai(){
+//     let name = "Vishnu"
+//     console.log(this.name);
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "Vishnu"
+//     console.group(this.username);
+// }
+// chai()
+
+// const chai = () =>{
+//     let username = "Vishnu"
+//     console.log(this.username);
+//     console.log(this);
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(5, 4));
+
+// const addTwo = (num1, num2) => num1 + num2
+// console.log(addTwo(9, 46))
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// console.log(addTwo(95, 5));
+
+const addTwo = (num1, num2) => ({username: "Vishnu"})
+console.log(addTwo())
 
