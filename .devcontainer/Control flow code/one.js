1ec3e082e91f7b23dd708eb39c88fe74ac3fdc7e
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2025-01-11 10:25:00 +0000

    one.js

---
+++ .devcontainer/Control flow code/one.js
@@ -0,0 +1,47 @@
+// if
+//const isUserloggedId = true
+const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less then 50");
+// }else {
+//     console.log("gretter than 50");
+// }
+
+// console.log("Etempreture");
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+//console.log(`User power: ${power}`);
+
+const balance = 1000
+
+//if (balance > 500) console.log("test"),console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than 500")
+// } else if(balance < 750) {
+//     console.log("less than 750")
+// } else if (balance < 900) {
+//     console.log("less than 900")
+// } else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = false
+const debitCard = true
+const loggedInFromEmail = true
+if(userLoggedIn && debitCard && 2==2){  // All true is true
+    console.log("Allow to buy course")
+}
+
+if(userLoggedIn || loggedInFromEmail){  // do me se  koi 1 true hona chahiye
+    console.log("User loghed in")
+}
