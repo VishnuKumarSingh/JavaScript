6d08e7c0090397b5e1e244332a1bc4d6727bc6e2
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2025-01-11 14:46:41 +0000

    switch.js

---
+++ .devcontainer/Control flow code/switch.js
@@ -0,0 +1,82 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 6
+
+// switch (month) {
+//     case 1:
+//         console.log("this month is January");
+//         break;
+//     case 2:
+//         console.log("this month is February");
+//         break;
+//     case 3:
+//         console.log("this month is March");
+//         break;
+//     case 4:
+//         console.log("this month is April");
+//         break;
+//     case 5:
+//         console.log("this month is May");
+//         break;
+//     case 6:
+//         console.log("this month is June");
+//         break;
+//     case 7:
+//         console.log("this month is July");
+//         break;
+//     case 8:
+//         console.log("this month is August");
+//         break;
+//     case 9:
+//         console.log("this month is September");
+//         break;
+//     case 10:
+//         console.log("this month is October");
+//         break;
+//     case 11:
+//         console.log("this month is Noveber");
+//         break;
+//     case 12:
+//         console.log("this month is December");
+//         break;
+    
+//     default:
+//         console.log("Rong month");
+//         break;
+// }
+
+const Week = 7
+
+switch (Week) {
+    case 1:
+        console.log("Today Monday")
+        break;
+    case 2:
+        console.log("Today Tuesday")
+        break;
+    case 3:
+        console.log("Today Wednesday")
+        break;
+    case 4:
+        console.log("Today Thursday")
+        break;
+    case 5:
+        console.log("Today Friday")
+        break;
+    case 6:
+        console.log("Today Saturday")
+        break;
+    case 7:
+        console.log("Today Sunday")
+        break;
+
+    default:
+        break;
+}
