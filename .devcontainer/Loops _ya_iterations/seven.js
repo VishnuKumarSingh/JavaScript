554a2685084205af6f8ebe9780804951f01224d6
VishnuKumarSingh <159816297+VishnuKumarSingh@users.noreply.github.com>
2025-01-15 13:52:21 +0000

    seven.js

---
+++ .devcontainer/Loops _ya_iterations/seven.js
@@ -0,0 +1,9 @@
+const mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//const newNums = mynumbers.map ( (num) => { return num + 10} )
+
+const newNums = mynumbers
+                .map( (num) => num * 10 )
+                .map( (num) => num + 1 )
+                .map( (num) => num >= 40 )
+console.log(newNums);
